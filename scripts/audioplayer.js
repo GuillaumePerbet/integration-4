db2b9e6e0454b4682859c1bbaa9adb7ffba6094f
Jeremy Mathieu <g3raphacs@gmail.com>
2020-05-23 16:06:28 +0200

    audioplayer script OK

--- scripts/audioplayer.js
+++ scripts/audioplayer.js
@@ -1,26 +1,77 @@
-// var playerList = document.getElementsByClassName("audioplayer");
-// var playerObjects =[];
+var playerList = document.getElementsByClassName("audioplayer");
+var playerObjects =[];
 
-// for (let i = 0; i < playerList.length; i++) {
+window.onload = initPlayers();
 
-//     let newPlayer = new Object();
-//     newPlayer.id = i;
-//     newPlayer.name = "audioPlayer"+(i+1);
-//     newPlayer.obj = playerList[i];
-//     newPlayer.audio = playerList[i].querySelector("audio");
-//     playerObjects.push(newPlayer);
-// }
+function initPlayers(){
+    for (let i = 0; i < playerList.length; i++) {
 
-// playerObjects.forEach(element => {
+        let newPlayer = new Object();
+        newPlayer.song = playerList[i].querySelector("audio");
+        newPlayer.songSlider = playerList[i].querySelector(".song-slider");
+        newPlayer.currentTime = playerList[i].querySelector('.current-time');
+        newPlayer.volumeSlider = playerList[i].querySelector('.volume-slider');
+        newPlayer.volumeBar = playerList[i].querySelector('.volume-time');
+        newPlayer.timerBar = playerList[i].querySelector('.slider-time');
+        newPlayer.volumeSlider.value = newPlayer.song.volume;
+        playerObjects.push(newPlayer);
+    }
+}
 
-//     console.log(element);
-// });
-// console.log(playerObjects)
-// playerObjects[0].audio.play();
+setInterval(updateSongSlider, 100);
 
-var audio = document.getElementById('audio');
-console.log(audio);
-var button = document.getElementById('play-button');
-button.addEventListener('click',function(){
-    audio.play();
-});
+function updateSongSlider () {
+    for (let i = 0; i < playerObjects.length; i++) {
+        var c =Math.round(playerObjects[i].song.currentTime);
+        playerObjects[i].songSlider.value = c;
+        playerObjects[i].currentTime.textContent = convertTime(c);
+        showDuration(i);
+    }
+}
+
+function convertTime (secs){
+    var min = Math.floor(secs/60);
+    var sec = secs % 60;
+    min = (min < 10) ? "0"+ min : min;
+    sec = (sec < 10) ? "0"+ sec : sec;
+    return (min + ":" + sec);
+}
+
+function showDuration (i){
+    var d = Math.floor(playerObjects[i].song.duration);
+    playerObjects[i].songSlider.setAttribute("max", d);
+    playerObjects[i].timerBar.style.width = String(Math.floor((75/d)*playerObjects[i].song.currentTime))+"px";
+}
+
+function playOrPauseSong(img,i){
+    showDuration(i);
+    if(playerObjects[i].song.paused){
+        playerObjects[i].song.play();
+        img.src = "media/audioplayer/pause.svg";
+    }
+    else{
+        playerObjects[i].song.pause();
+        img.src = "media/audioplayer/play.svg";
+    }
+}
+
+function seekSong(i){
+    playerObjects[i].song.currentTime = playerObjects[i].songSlider.value;
+    playerObjects[i].currentTime.textContent = convertTime(playerObjects[i].song.currentTime);
+}
+function adjustVolume(i){
+    playerObjects[i].song.volume= playerObjects[i].volumeSlider.value;
+    playerObjects[i].volumeBar.style.width = String(Math.floor(50*playerObjects[i].song.volume))+"px";
+}
+
+function muteOrUnmute(img,i){
+    if(playerObjects[i].song.muted){
+        img.src = "media/audioplayer/VolumeOn.svg";
+        adjustVolume(i)
+    }
+    else{
+        img.src = "media/audioplayer/VolumeOff.svg";
+        playerObjects[i].volumeBar.style.width = "0px";
+    }
+    playerObjects[i].song.muted = !playerObjects[i].song.muted;
+}
