7cef2a666085bf5a0b7f352207489368cd0e8efb
Guillaume Perbet <perbet.dev@gmail.com>
2020-05-25 14:07:36 +0200

    deleting test logs

--- scripts/menu.js
+++ scripts/menu.js
@@ -2,7 +2,6 @@ const button=document.getElementById('menu-button');
 const menu=document.getElementById('menu');
 
 button.addEventListener('click',()=>{
-    console.log (window.getComputedStyle(menu).display);
     switch(window.getComputedStyle(menu).display){
         case 'flex':
             menu.style.display='none';
